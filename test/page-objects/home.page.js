2a084a6123772eee85b423ee90bf2174b1b00b4a
Daniel Webb <39187517+dhansak79@users.noreply.github.com>
2025-04-11 12:07:50 +0100

    correct assertion (#4)
    Co-authored-by: Daniel Webb <daniel.webb@esynergy.co.uk>

--- test/page-objects/home.page.js
+++
@@ -1,9 +0,0 @@
-import { Page } from 'page-objects/page'
-
-export class HomePage extends Page {
-  async open() {
-    return await super.open('')
-  }
-}
-
-export default new HomePage()
--- test/steps/stepdefs.js
+++ test/steps/stepdefs.js
@@ -1,5 +1,4 @@
 import { Given, When, Then } from '@cucumber/cucumber'
-import { HomePage } from 'page-objects/home.page.js'
 import { expect } from 'chai'
 import { browser } from '@wdio/globals'
 
@@ -16,6 +15,5 @@ When(
 )
 
 Then('the user is on the home page', async () => {
-  const pageHeading = await HomePage.pageHeading.getText()
-  expect(pageHeading).to.equal('Home')
+  expect(await browser.$('h1').getText()).to.equal('Home')
 })
