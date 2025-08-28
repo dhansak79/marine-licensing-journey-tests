b42c4fac4034df770fa447ed11f0b0ada5116adc
Vasudev Ganesan <vasudev.ganesan@esynergy.co.uk>
2025-08-28 18:43:03 +0100

    Update index.js

--- test-infrastructure/capture/index.js
+++ test-infrastructure/capture/index.js
@@ -2,4 +2,8 @@ export { attachRichFeatureContext } from './feature-context.js'
 export { attachJson } from './json.js'
 export { logOperation, logUserCleanup } from './operational.js'
 export { takePlaywrightScreenshot } from './playwright-screenshot.js'
-export { takeScreenshot } from './screenshot.js'
+export {
+  takeScreenshot,
+  takeEnhancedScreenshot,
+  logCurrentPageInfo
+} from './screenshot.js'
